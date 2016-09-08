6b1fcfdd4e10f147fc7551e14dc65653e5012b8d
Zed-chi <zed-chi@mail.ru>
2016-09-09 00:46:46 +0300

    разного
    всякого

--- js/script.js
+++ js/script.js
@@ -39,7 +39,6 @@ function getData(){
     load.style.visibility = "visible";
     $.ajax({
     url:"https://api.jqestate.ru/v1/properties/country?pagination[limit]="+limit+"&pagination[offset]="+offset })
-    
     .done(function( data ) {
         //vars
         var length  = data.items.length;
@@ -52,33 +51,37 @@ function getData(){
         
         //stuff
         x.innerHTML="";
-        load.style.visibility = "hidden";
+        
         
         //main
         if(data.items.length>0){
             for(var i=0; i<length; i++){
                 
-                var route   = data.items[i].location.routeName ? data.items[i].location.routeName+" ш" : "";
-                var settle  = data.items[i].location.settlementName ? data.items[i].location.settlementName : "";
-                var dist    = data.items[i].location.mkadDistance ? data.items[i].location.mkadDistance+" км" : "";
-                var price   = data.items[i].saleOffer ? data.items[i].saleOffer.price + " " + data.items[i].saleOffer.currency : "";
-                var meters  = data.items[i].specification.area ? data.items[i].specification.area + " м<sup>2</sup>" :"";
-                var sotki   = data.items[i].landDetails.area ? data.items[i].landDetails.area + " сот" : "";
+                var route   = data.items[i].location.routeName ? data.items[i].location.routeName+" ш." : " ";
+                var dist    = data.items[i].location.mkadDistance ? data.items[i].location.mkadDistance+" км." : "";
+                var settle  = data.items[i].location.settlementName ? data.items[i].location.settlementName + ", " + dist : " ";
+                
+                var price   = data.items[i].saleOffer ? "<span class='space'>" + data.items[i].saleOffer.price + " " + data.items[i].saleOffer.currency + " " +"</span>" + "<div class='border'></div>": "";
+                var meters  = data.items[i].specification.area ? "<span class='space'>" + data.items[i].specification.area + " м<sup>2</sup> " +"</span>" + "<div class='border'></div>" : "";
+                var sotki   = data.items[i].landDetails.area ? "<span class='space'>" + data.items[i].landDetails.area + " сот " + "</span>" + "<div class='border'></div>" : "";
                 var blank   = "http://www.hdi-slc.com/wp-content/uploads/2012/07/blank-profile.jpg";
                 var img     = data.items[i].images[0] ? data.items[i].images[0].url : blank;
                 
                 x.innerHTML+="<div class='card'>"+
                 "<img src='"+img+"' />"+
                 "<p class='route'>"+route+"</p>"+
-                "<p class='settle'>"+settle+", "+dist+"</p>"+
-                "<p class='info'>"+price+" "+"</p>"+
-                "<p class='info'>"+sotki+" "+"</p>"+
-                "<p class='info'>"+meters+"</p>"+
+                "<p class='settle'>" + settle + "</p>" +
+                "<div class='info'>" + price + "</div>" +
+                "<div class='info'>"+ sotki + "</div>" +
+                "<div class='info'>" + meters + "</div>" +
                 "</div>";
             }
+
+
         }
         else{alert("fail!");}
+        document.onload=load.style.visibility = "hidden";
     })
-    
     .fail(function(){alert("Нет объектов");});
+    
 }
